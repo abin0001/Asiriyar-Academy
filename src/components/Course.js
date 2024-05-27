5f32a99d067a32c9c050de65c99ca6142265205c
imisgsc <simigracia@gmail.com>
2024-05-27 19:31:38 +0530

    Fixed Opacity

--- src/components/Course.js
+++ src/components/Course.js
@@ -20,15 +20,14 @@ const Course = () => {
   const displayedCourses = showMore ? courses : courses.slice(0, 5);
 
   return (
-    <div className="p-10">
+    <div>
       <h1 className="text-4xl font-bold mb-8 text-center">Our Popular Courses</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
         {displayedCourses.map(course => (
           <div 
             key={course.id} 
-            className="relative group overflow-hidden rounded-lg shadow-lg cursor-pointer transform transition-transform duration-300 hover:scale-105"
-          >
-            <img src={courseImage} alt={course.title} className="w-full h-100 object-cover opacity-80 group-hover:opacity-50 transition-opacity duration-300" />
+            className="relative group overflow-hidden rounded-lg shadow-lg cursor-pointer transform transition-transform duration-300 hover:scale-105">
+            <img src={courseImage} alt={course.title} className="w-full h-100 mb-10 object-cover opacity-80 group-hover:opacity-100 transition-opacity duration-300" />
             <div className="absolute inset-0 flex flex-col justify-end p-4 bg-gradient-to-t from-black via-transparent to-transparent">
               <h3 className="text-lg font-semibold text-white">{course.title}</h3>
               <p className="text-sm text-gray-300">{course.description}</p>
@@ -41,7 +40,7 @@ const Course = () => {
         <div className="text-center mt-8">
           <button 
             onClick={() => setShowMore(true)} 
-            className="text-lg font-semibold text-white bg-orange-500 rounded-lg px-6 py-3 hover:bg-orange-600"
+            className="text-lg font-semibold mb-10 text-white bg-orange-500 rounded-lg px-6 py-3 hover:bg-orange-600"
           >
             View more
           </button>
