6079a336ddca8145db684faa4a2327591602e048
imisgsc <simigracia@gmail.com>
2024-05-27 16:30:00 +0530

    Completed About us

--- src/components/About.js
+++ src/components/About.js
@@ -16,6 +16,35 @@ const About = () => {
           <button className="text-lg font-semibold text-white bg-orange-500 rounded-lg px-5 py-2 mt-2 mb-2">Contact Us</button>
         </nav>
         </header>
+        <main>
+        <div className="p-10">
+        <div className="flex flex-col md:flex-row items-center justify-between">
+          <div className="flex-1 md:mr-10">
+            <h1 className="text-4xl font-bold mt-0 mb-10">About Us</h1>
+            <p className="text-lg mb-10">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div className="bg-orange-500 text-white p-6 rounded-lg">
+              <p className="text-center font-semibold mb-2">Our Vision</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+              <div className="bg-white border-2 border-orange-500 text-orange-500 p-6 rounded-lg flex flex-col items-center">
+                <p className="text-center font-semibold mb-2">Our Mission</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex-1 mt-10 md:mt-0">
+            <img src={about} alt="About Us" className="w-full h-auto rounded-full border-4 border-orange-500" />
+          </div>
+        </div>
+      </div>
+        </main>
       
     </div>
   )
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -19,8 +19,8 @@ const Landing = () => {
         <main className="flex flex-1 justify-between items-center p-6 bg-gray-50">
         <div className="max-w-lg">
           <h2 className="text-orange-500 text-xl font-bold mb-2 ml-20">LEARN FROM TODAY</h2>
-          <h1 className="text-4xl font-bold mb-4 mt-8 ml-20">THE BEST WAY FOR YOUR LEARNING</h1>
-          <p className="text-gray-700 mb-6 ml-20 font-semibold">
+          <h1 className="text-4xl font-bold mb-8 mt-8 ml-20">THE BEST WAY FOR YOUR LEARNING</h1>
+          <p className="text-gray-700 mb-10 ml-20 font-semibold">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
           <div className="space-x-4">
