012b29cad504e1adb8da8aa66be6450fcacc62be
imisgsc <simigracia@gmail.com>
2024-05-28 11:01:18 +0530

    commit morning

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ function App() {
             <img src={logo} alt="Logo" className="w-15  ml-3 mt-4" />
           </div>
           <nav className="flex space-x-20">
-            <Link to="/" className="text-lg font-semibold hover:text-orange-700 mt-4" >Home</Link>
-            <Link to="/about" className="text-lg font-semibold hover:text-orange-700 mt-4">About Us</Link>
-            <Link to="/course" className="text-lg font-semibold hover:text-orange-700 mt-4">Course</Link>
+            <Link to="/" className="text-lg font-bold hover:text-orange-700 mt-4" >Home</Link>
+            <Link to="/about" className="text-lg font-bold hover:text-orange-700 mt-4">About Us</Link>
+            <Link to="/course" className="text-lg font-bold hover:text-orange-700 mt-4">Course</Link>
             {/* Link the "Contact Us" button to the Contact page */}
-            <Link to="/contact" className="text-lg font-semibold text-white bg-orange-500 rounded-lg px-5 py-2 mt-2 mb-2 hover:bg-orange-600">Contact Us</Link>
+            <Link to="/contact" className="text-lg font-bold text-white bg-orange-500 rounded-lg px-5 py-2 mt-2 mb-2 hover:bg-orange-600">Contact Us</Link>
           </nav>
         </header>
         <Routes>
